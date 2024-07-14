1b76ceeb09436063190153567d9c2d09d7eed975
Annibal Silva <hannibaldaturn@gmail.com>
2024-07-14 01:57:02 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node'
+}
+
+module.exports = config
