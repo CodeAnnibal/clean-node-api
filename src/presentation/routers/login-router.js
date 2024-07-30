2ea72a2e49654df82d38f262cd02040d237b89fb
Annibal Silva <hannibaldaturn@gmail.com>
2024-07-29 23:26:58 -0300

    add jest configurations

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -26,6 +26,7 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ acessToken })
     } catch (error) {
+      console.error(error)
       return HttpResponse.serverError()
     }
   }
